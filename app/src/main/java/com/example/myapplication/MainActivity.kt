ffd26c4ae7d9bc7fb58486adc71105dd420b8881
iffy <zyf8395@sina.com>
2020-01-04 12:53:04 +0800

    linkToDeath

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.ComponentName
 import android.content.Intent
 import android.content.ServiceConnection
+import android.os.Binder
 import android.os.Bundle
 import android.os.IBinder
 import com.google.android.material.snackbar.Snackbar
@@ -15,43 +16,62 @@ import com.example.myapplication.IMyAidlInterface.Stub.getDefaultImpl
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), ServiceConnection {
+    var mbinder: IBinder? = null
+    lateinit var mIMyAidlInterface: IMyAidlInterface
     override fun onServiceDisconnected(name: ComponentName?) {
         println("onServiceDisconnected")
     }
 
     override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+        mbinder = service
         println("onServiceConnected")
         //直接操作Binder
-        val _data = android.os.Parcel.obtain()
-        val _reply = android.os.Parcel.obtain()
-        val _result: Boolean
-        try {
-            _data.writeInterfaceToken("com.example.myapplication.IMyAidlInterface")
-            _data.writeString("iffy1")
-            val _status =
-                service!!.transact(IMyAidlInterface.Stub.TRANSACTION_getService, _data, _reply, 0)
-            _reply.readException()
-            _result = 0 != _reply.readInt()
-        } finally {
-            _reply.recycle()
-            _data.recycle()
-        }
-
-        if (_result) {
-            println("找到了iffy1")
-        } else {
-            println("没找到iffy1")
-        }
-
-
-//        val mIMyAidlInterface = IMyAidlInterface.Stub.asInterface(service)//Binder转化为 Interface(Stub.proxy)
-//        println("得到类 $mIMyAidlInterface")
-//        if (mIMyAidlInterface.getService("iffy")) {
-//            println("找到了iffy")
+//        val _data = android.os.Parcel.obtain()
+//        val _reply = android.os.Parcel.obtain()
+//        val _result: Boolean
+//        try {
+//            _data.writeInterfaceToken("com.example.myapplication.IMyAidlInterface")
+//            _data.writeString("iffy1")
+//            val _status =
+//                service!!.transact(IMyAidlInterface.Stub.TRANSACTION_getService, _data, _reply, 0)
+//            _reply.readException()
+//            _result = 0 != _reply.readInt()
+//        } finally {
+//            _reply.recycle()
+//            _data.recycle()
+//        }
+//
+//        if (_result) {
+//            println("找到了iffy1")
 //        } else {
-//            println("没找到iffy")
+//            println("没找到iffy1")
 //        }
 
+//常规用法
+        mIMyAidlInterface =
+            IMyAidlInterface.Stub.asInterface(service)//Binder转化为 Interface(Stub.proxy)
+        println("得到类 $mIMyAidlInterface")
+        if (mIMyAidlInterface.getService("iffy")) {
+            println("找到了iffy")
+        } else {
+            println("没找到iffy")
+        }
+
+        //死亡代理用来监听Server端状态
+        //adb shell am force-stop com.example.iffyservice 来验证
+        service?.linkToDeath(object : IBinder.DeathRecipient {
+            override fun binderDied() {
+                println("那位挂了")
+                mIMyAidlInterface.asBinder().unlinkToDeath(this, 0)
+               // bindService()
+            }
+        }, 0)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unbindService(this)
     }
 
 
@@ -63,31 +83,27 @@ class MainActivity : AppCompatActivity(), ServiceConnection {
         fab.setOnClickListener {
             Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-            val i = Intent("com.example.iffyservice.Iffyservice")
-            i.setPackage("com.example.iffyservice")
-            if (bindService(i, this, Service.BIND_AUTO_CREATE)) {
-                println("绑定成功")
+            bindService()
+        }
+        //kotlin-android-extensions 可以直接操作控件id
+        talk_to_service.setOnClickListener {
+            if (mIMyAidlInterface.getService("ttt")) {
+                println("找到了ttt")
             } else {
-                println("绑定失败")
+                println("没找到ttt")
             }
 
-
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    fun bindService() {
+        val i = Intent("com.example.iffyservice.Iffyservice")
+        i.setPackage("com.example.iffyservice")
+        if (bindService(i, this, Service.BIND_AUTO_CREATE)) {
+            println("绑定成功")
+        } else {
+            println("绑定失败")
         }
     }
+
 }
