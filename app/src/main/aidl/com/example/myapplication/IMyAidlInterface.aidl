fbd1092c70d0f43dd28d91866aba35ba003b0bbb
iffy <zyf8395@sina.com>
2020-01-17 11:14:21 +0800

    parcelable in out inout

--- app/src/main/aidl/com/example/myapplication/IMyAidlInterface.aidl
+++ app/src/main/aidl/com/example/myapplication/IMyAidlInterface.aidl
@@ -9,8 +9,14 @@ interface IMyAidlInterface {
         void SendStudent(out Student s);
 
         //inout测试
+
+        //in 代表C->S S对Student的修改不会作用到C端的实例
         void addStudentIn(in Student s);
+
+        //out 代表S->C C端的内容不能传递到S端，S端会新建一个student并且赋值给C端的student
         void addStudentOut(out Student s);
+
+        //inout双向传递
         void addStudentInOut(inout Student s);
 }
 
--- app/src/main/java/com/example/myapplication/Student.kt
+++ app/src/main/java/com/example/myapplication/Student.kt
@@ -3,31 +3,28 @@ package com.example.myapplication
 import android.os.Parcel
 import android.os.Parcelable
 
-class Student(var nane: String?,var age: Int) : Parcelable {
+class Student(var nane: String?, var age: Int) : Parcelable {
     //自己实现
-    constructor():this("",1)
-
-    constructor(parcel: Parcel) : this() {
-        nane = parcel.readString()
-        age = parcel.readInt()
-    }
-
-    //自己实现
-    fun readFromParcel(parcel: Parcel){
-        nane = parcel.readString()
-        age = parcel.readInt()
-    }
-
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(nane)
-        parcel.writeInt(age)
-    }
+    constructor() : this("", 1)
 
     override fun describeContents(): Int {
         return 0
     }
 
+    /**反序列化*/
+    constructor(parcel: Parcel) : this() {
+        nane = parcel.readString()
+        age = parcel.readInt()
+    }
+
+    /**序列化*/
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        //序列化和反序列化读写顺序要一致
+        parcel.writeString(nane)
+        parcel.writeInt(age)
+    }
+
+    /**反序列化*/
     companion object CREATOR : Parcelable.Creator<Student> {
         override fun createFromParcel(parcel: Parcel): Student {
             return Student(parcel)
